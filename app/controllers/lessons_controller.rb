46195a5a26b2f0636a5cd4cbfb45ffbdabd81859
Samir Deeb <samirndeeb@gmail.com>
2019-10-14 18:30:28 +0000

    Lessons Error

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,16 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
+    @lesson = Lesson.show
+  end
+
+  def create
+    @lesson = current_lesson.section.course(lesson_params)
+    if current_user.enrolled_in? = false
+      redirect_to course_path, alert: 'Invalid User'
+    end
   end
 
   private
