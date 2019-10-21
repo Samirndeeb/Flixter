3a0fdd193845eb627a4261270d50bb7d8b7e7020
Samir Deeb <samirndeeb@gmail.com>
2019-10-21 15:33:50 +0000

    JavaScript

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,7 +3,6 @@ class LessonsController < ApplicationController
   before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
-    @lesson = Lesson.show
   end
 
   def create
@@ -13,7 +12,7 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    if current_user.enrolled_in? == false
+    if current_user.enrolled_in?(current_lesson.section.course) == false
       redirect_to course_path, alert: 'Invalid User'
     end
   end
